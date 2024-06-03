5fd6aefe5e39ed790d7126d948aae573467f5a52
takiy-bdg <isaac-web.bigdataghana@outlook.com>
2024-06-03 17:37:25 +0000

    Fix background colors in positions page

--- client/src/pages/Positions.tsx
+++ client/src/pages/Positions.tsx
@@ -14,59 +14,63 @@ import { DataItem, Investor, InvestorAction, Wallet } from '../types';
 
 const Positions = () => {
   return (
-    <section className="container mx-auto px-10">
-      <TopNav />
-
-      <div className="py-5 pb-10">
-        <h1 className="text-3xl font-bold">Positions</h1>
-      </div>
-      <div className="flex flex-row flex-nowrap overflow-x-auto mb-5 gap-5">
-        <button className="px-5 py-2 font-semibold bg-purple-800">
-          Add Wallet
-        </button>
-        <button className="px-5 py-2 font-semibold bg-purple-800">
-          Update Wallet
-        </button>
-        <button className="px-5 py-2 font-semibold bg-purple-800">
-          Delete Wallet
-        </button>
-        <button className="px-5 py-2 font-semibold bg-purple-800">
-          Add Investor
-        </button>
-        <button className="px-5 py-2 font-semibold bg-purple-800">
-          Update Investor
-        </button>
-        <button className="px-5 py-2 font-semibold bg-purple-800">
-          Delete Investor
-        </button>
+    <section className="container mx-auto p-10">
+      <div className="mb-8">
+        <TopNav />
       </div>
 
-      <div className="grid grid-cols-12 gap-5 mb-6">
-        <div className="col-span-5 min-h-[20em] border border-white/10 rounded-sm overflow-auto p-5">
-          <AppTable<Wallet>
-            columns={walletsTableColumn}
-            data={walletsTableSampleData}
-          />
+      <div className="bg-[#6558F5] p-10">
+        <div className="py-5 pb-10">
+          <h1 className="text-3xl font-bold text-white">Positions</h1>
         </div>
-        <div className="col-span-7 min-h-[20em] border border-white/10 rounded-sm p-5">
-          <AppTable<Investor>
-            columns={investorTableColumn}
-            data={investorTableSampleData}
-          />
+        <div className="flex flex-row flex-nowrap overflow-x-auto mb-5 gap-5">
+          <button className="px-5 py-2 font-semibold bg-[#730FC3] text-white">
+            Add Wallet
+          </button>
+          <button className="px-5 py-2 font-semibold bg-[#730FC3] text-white">
+            Update Wallet
+          </button>
+          <button className="px-5 py-2 font-semibold bg-[#730FC3] text-white">
+            Delete Wallet
+          </button>
+          <button className="px-5 py-2 font-semibold bg-[#730FC3] text-white">
+            Add Investor
+          </button>
+          <button className="px-5 py-2 font-semibold bg-[#730FC3] text-white">
+            Update Investor
+          </button>
+          <button className="px-5 py-2 font-semibold bg-[#730FC3] text-white">
+            Delete Investor
+          </button>
         </div>
-      </div>
-      <div className="grid grid-cols-12 gap-5">
-        <div className="col-span-7 min-h-[20em] border border-white/10 rounded-sm overflow-auto p-5">
-          <AppTable<DataItem>
-            columns={positionsTableColumn}
-            data={positionsTableSampleData}
-          />
+
+        <div className="grid grid-cols-12 gap-5 mb-6">
+          <div className="col-span-5 min-h-[20em] bg-[#F7C325] rounded-sm overflow-auto p-5">
+            <AppTable<Wallet>
+              columns={walletsTableColumn}
+              data={walletsTableSampleData}
+            />
+          </div>
+          <div className="col-span-7 min-h-[20em] bg-[#F7C325] rounded-sm p-5">
+            <AppTable<Investor>
+              columns={investorTableColumn}
+              data={investorTableSampleData}
+            />
+          </div>
         </div>
-        <div className="col-span-5 min-h-[20em] border border-white/10 rounded-sm p-5">
-          <AppTable<InvestorAction>
-            columns={investorActionTableColumn}
-            data={investorActionTableSampleData}
-          />
+        <div className="grid grid-cols-12 gap-5">
+          <div className="col-span-7 min-h-[20em] bg-[#F7C325] rounded-sm overflow-auto p-5">
+            <AppTable<DataItem>
+              columns={positionsTableColumn}
+              data={positionsTableSampleData}
+            />
+          </div>
+          <div className="col-span-5 min-h-[20em] bg-[#F7C325] rounded-sm p-5">
+            <AppTable<InvestorAction>
+              columns={investorActionTableColumn}
+              data={investorActionTableSampleData}
+            />
+          </div>
         </div>
       </div>
     </section>
