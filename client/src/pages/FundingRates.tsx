a7877f9fb23e15cd0611c428613817d5e8b615b4
Nathan Turkson <nathanturkson@gmail.com>
2024-06-03 17:22:23 +0000

    Implemented Funding Rates

--- client/src/pages/FundingRates.tsx
+++ client/src/pages/FundingRates.tsx
@@ -1,10 +1,60 @@
+import ListItem from "../components/ListItem";
+import TopNav from "../components/TopNav";
 
 const FundingRates = () => {
   return (
-    <div>
-      Fundng Rates
-    </div>
-  )
-}
+    <div className="p-10">
+      <header>
+        <TopNav />
+      </header>
+      <main className="bg-[#6558F5] text-white h-[80vh] mt-5 rounded-md">
+        <h1 className="text-center font-bold py-4 text-2xl">Funding Rates</h1>
 
-export default FundingRates
+        <ul className="flex gap-4  items-center mx-3">
+          <li className=" h-[50px]">
+            <select
+              id="countries"
+              className="bg-[#207868]  text-white text-sm rounded-lg h-full focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
+            >
+              <option selected>Token </option>
+              <option value="US">United States</option>
+              <option value="CA">Canada</option>
+              <option value="FR">France</option>
+              <option value="DE">Germany</option>
+            </select>
+          </li>
+          <ListItem title="Minimum funding rate(yearly)" />
+          <ListItem title="Exchanges" />
+          <button className="bg-[#730FC3] p-3 rounded-md ">Go!</button>
+        </ul>
+
+        <section className="grid grid-cols-12 gap-4 h-[65vh] p-5 ">
+          <div className="border col-span-full md:col-span-3 bg-[#F7C325] h-full rounded-md text-center">
+            <h2 className="mt-5 text-black text-center font-bold">
+              Table Results
+            </h2>
+
+            <button>Exchange</button>
+          </div>
+          <div className="border col-span-full md:col-span-6  h-full rounded-md flex flex-col gap-4">
+            <div className="bg-[#F7C325] h-1/2">
+              
+              <h2 className="text-center text-black font-bold">Funding history chart (from selected row in table)</h2>
+            </div>
+            <div className="bg-[#F7C325] h-1/2">
+              {" "}
+              <h2 className="text-center text-black font-bold"> Price chart (from selected row in table)</h2>
+            </div>
+          </div>
+          <div className="border col-span-full md:col-span-3 bg-[#F7C325] h-full rounded-md">
+            <h2 className="mt-5 text-black text-center font-bold">
+              Order Book (from Selected row in table)
+            </h2>
+          </div>
+        </section>
+      </main>
+    </div>
+  );
+};
+
+export default FundingRates;
