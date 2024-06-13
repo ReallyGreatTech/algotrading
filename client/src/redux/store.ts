d4957791add2f6918904a6a0f2bb4bad33a4e692
Nathan Turkson <nathanturkson@gmail.com>
2024-06-13 12:42:18 +0000

    updated time filter logic

--- client/src/redux/store.ts
+++ client/src/redux/store.ts
@@ -3,12 +3,15 @@ import { configureStore } from "@reduxjs/toolkit";
 import tokenReducer from "./features/tokens/tokenSlice";
 import fundingHistoryReducer from "./features/fundingHistory/fundingHistorySlice";
 import marketReducer from "./features/market/marketSlice";
+import timeFilterReducer from "./features/timeFilter/timeFilter";
 
 const store = configureStore({
   reducer: {
     token: tokenReducer,
     fundingHistory: fundingHistoryReducer,
     market: marketReducer,
+    timefilter: timeFilterReducer
+    
   },
 });
 
