273acbb900f352237ee789bace7c44f68f646fed
unknown <amlanujerry99@gmail.com>
2024-07-12 08:18:39 +0000

    updated env variables

--- client/src/redux/api/apiClient.ts
+++ client/src/redux/api/apiClient.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
 export const apiClient = axios.create({
-  baseURL: 'http://3.76.134.149:8000/api',
+  // baseURL: 'http://3.76.134.149:8000/api',
+  baseURL: import.meta.env.VITE_ALGO_TRADING_API_URL,
 });
