55305cbbe991b63a4bb931d517a6a1297a6a17c1
unknown <amlanujerry99@gmail.com>
2024-07-29 12:47:03 +0000

    fixing build issue

--- client/src/redux/features/positions/positionsSlice.ts
+++ client/src/redux/features/positions/positionsSlice.ts
@@ -1,6 +1,7 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { PositionsGroup } from '../../../types';
-import { fetchPositions, updatePosition } from '../../api/positions';
+// import { fetchPositions, updatePosition } from '../../api/positions';
+import { fetchPositions } from '../../api/positions';
 
 interface PositionsState {
   loading: boolean;
