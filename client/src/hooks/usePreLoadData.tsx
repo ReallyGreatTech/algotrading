59fbdb3907c332307142a91323b9ec1e62895d0e
takiy-bdg <isaac-web.bigdataghana@outlook.com>
2024-08-01 15:37:12 +0000

    clean up usePreLoadData Hook

--- client/src/hooks/usePreLoadData.tsx
+++ client/src/hooks/usePreLoadData.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { loadTokens } from '../redux/api/tokens';
 import { _fetchMarketOptions } from '../redux/api/marketsOptions';
 
