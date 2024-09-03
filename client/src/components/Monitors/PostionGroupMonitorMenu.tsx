1993e923cc603dbf0e23ae876f49f70dcc41b7b3
Isaac-web <kanytakiy@gmail.com>
2024-09-03 12:53:04 +0000

    Modify monitors ttooltip to match figma design

--- client/src/components/Monitors/PostionGroupMonitorMenu.tsx
+++ client/src/components/Monitors/PostionGroupMonitorMenu.tsx
@@ -1,14 +1,14 @@
-import { Button, Popover } from "antd";
-import { HiMiniQuestionMarkCircle } from "react-icons/hi2";
-import { IoMdAddCircle } from "react-icons/io";
-import { FaEye } from "react-icons/fa";
-import {   PositionGroupMonitor , PositionsGroup } from "../../types";
-import { useEffect, useState } from "react";
-import { useAppDispatch, useAppSelector } from "../../hooks";
-import { fetchPositionMonitors } from "../../redux/api/position-monitors";
-import CreatePositionGroupMonitor from "../Dialogs/CreatePositionGroupMonitor";
-import ViewPositionGroupMonitorDialog from "../Dialogs/ViewPositionGroupMonitorDialog";
-import { MdMonitorHeart } from "react-icons/md";
+import { Button, Popover } from 'antd';
+import { HiMiniQuestionMarkCircle } from 'react-icons/hi2';
+import { IoMdAddCircle } from 'react-icons/io';
+import { FaEye } from 'react-icons/fa';
+import { PositionGroupMonitor, PositionsGroup } from '../../types';
+import { useEffect, useState } from 'react';
+import { useAppDispatch, useAppSelector } from '../../hooks';
+import { fetchPositionMonitors } from '../../redux/api/position-monitors';
+import CreatePositionGroupMonitor from '../Dialogs/CreatePositionGroupMonitor';
+import ViewPositionGroupMonitorDialog from '../Dialogs/ViewPositionGroupMonitorDialog';
+import { MdMonitorHeart } from 'react-icons/md';
 
 interface MonitorMenuProps {
   positionGroup: PositionsGroup;
@@ -24,11 +24,10 @@ const PositionGroupMonitorMenu = ({
   fieldValue,
 }: MonitorMenuProps) => {
   const [editMonitorDialogOpen, setEditMonitorDialogOpen] = useState(false);
-   const [viewMonitorDialogOpen, setViewMonitorDialogOpen] = useState(false);
+  const [viewMonitorDialogOpen, setViewMonitorDialogOpen] = useState(false);
   const positionGroupMonitors = useAppSelector(
     (state) => state.positionGroupMonitors.data
   );
-
 
   const [selectedMonitors, setSelectedMonitors] = useState<
     PositionGroupMonitor[]
@@ -46,63 +45,68 @@ const PositionGroupMonitorMenu = ({
         monitor.on_field === onField && monitor.token === positionGroup.token
     );
     setSelectedMonitors(filteredMonitors);
-  }, [positionGroup.token,positionGroupMonitors, onField]);
+  }, [positionGroup.token, positionGroupMonitors, onField]);
 
   return (
     <>
       <Popover
-        color="#334154"
-        placement="rightBottom"
+        color="#232C39"
+        placement="rightTop"
+        defaultOpen
         content={
           <div
             onClick={(e) => e.stopPropagation()}
-            className="bg-[#334154] p-4 text-white"
+            className="text-white -mt-3 -ml-3 -mr-3 -mb-3 overflow-hidden rounded-lg"
           >
-            <div className="my-3">
-              <div className="flex border-b border-primary-light/80 text-sm items-center mb-3 gap-1 pb-1">
-                <h4 className="text-center text-lg font-bold">
+            <div>
+              <div className="flex bg-[#232C39] text-sm items-center justify-between mb-3 gap-1 px-3 py-2 pb-1">
+                <h4 className="text-center font-semibold">
                   Position Group Monitors
                 </h4>
-                <HiMiniQuestionMarkCircle fontSize={"18px"} />
+                <HiMiniQuestionMarkCircle fontSize={'15px'} />
               </div>
 
-              <div className="flex flex-col gap-1">
-                <h3 className="text-sm">
-                  <span className="font-bold">Field: </span> {fieldLabel}
-                </h3>
-                <p className="text-sm">
-                  <span className="font-bold">Value:</span> {fieldValue}
-                </p>
-                <p className="text-sm">
-                  <span className="font-bold">Monitors Set: </span>{" "}
-                  {selectedMonitors.length}
-                </p>
-              </div>
-            </div>
+              <div className="px-4 bg-[#324054] flex flex-col gap-5 py-4">
+                <div className="flex flex-col gap-2 lg:min-w-56">
+                  <h3 className="text-sm">
+                    <span className="font-bold">Field: </span> {fieldLabel}
+                  </h3>
+                  <p className="text-sm">
+                    <span className="font-bold">Value:</span> {fieldValue}
+                  </p>
+                  <p className="text-sm">
+                    <span className="font-bold">Monitors Set: </span>{' '}
+                    {selectedMonitors.length}
+                  </p>
+                </div>
 
-            <div className="flex gap-1">
-              <Button
-                icon={<FaEye />}
-                onClick={(e) => {
-                  e.stopPropagation();
-                  hide(); // Close the Popover
-                  // dispatch(selectPosition(position));
-                  setViewMonitorDialogOpen(true); // Open the dialog
-                }}
-              >
-                View
-              </Button>
-              <Button
-                icon={<IoMdAddCircle />}
-                onClick={(e) => {
-                  e.stopPropagation();
-                  hide(); // Close the Popover
-                  dispatch(fetchPositionMonitors());
-                  setEditMonitorDialogOpen(true); // Open the dialog
-                }}
-              >
-                Add
-              </Button>
+                <div className="flex gap-4 justify-end">
+                  <button
+                    onClick={(e) => {
+                      e.stopPropagation();
+                      hide(); // Close the Popover
+                      // dispatch(selectPosition(position));
+                      setViewMonitorDialogOpen(true); // Open the dialog
+                    }}
+                    className="flex justify-center items-center gap-2 px-3 py-1 rounded-lg border-2 border-primary bg-[#121C2D] text-white shadow-primary"
+                  >
+                    <FaEye />
+                    <span>View</span>
+                  </button>
+                  <button
+                    onClick={(e) => {
+                      e.stopPropagation();
+                      hide(); // Close the Popover
+                      dispatch(fetchPositionMonitors());
+                      setEditMonitorDialogOpen(true); // Open the dialog
+                    }}
+                    className="flex justify-center items-center gap-2 px-4 py-1 bg-primary rounded-lg text-white shadow-primary"
+                  >
+                    <IoMdAddCircle />
+                    <span>Add</span>
+                  </button>
+                </div>
+              </div>
             </div>
           </div>
         }
@@ -110,12 +114,12 @@ const PositionGroupMonitorMenu = ({
         open={open}
         onOpenChange={setOpen}
       >
-
         <div className="inline-flex gap-2 items-center justify-center px-1">
-        <span onClick={() => setOpen(!open)}>{fieldValue}</span>
-       {selectedMonitors.length ? <MdMonitorHeart className="text-green-400"  />:null} 
+          <span onClick={() => setOpen(!open)}>{fieldValue}</span>
+          {selectedMonitors.length ? (
+            <div className="bg-green-400 h-2 w-2 rounded-full" />
+          ) : null}
         </div>
-       
       </Popover>
 
       <CreatePositionGroupMonitor
